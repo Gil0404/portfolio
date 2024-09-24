830b34b73612289d1934721f59541be0130e3558
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-09-24 08:39:12 +0800

    added the frame in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center bg-teal-950 h-screen w-screen sm:items-start"></main>
+    <main className="flex flex-col gap-8 row-start-2 items-center bg-teal-950 h-screen w-screen sm:items-start">
+      <div className="flex flex-row h-screen max-w-7xl w-screen mx-auto bg-cyan-200">
+        <div className="flex flex-col w-1/4 h-screen  bg-slate-950 border-2 rounded-md border-cyan-500 justify-start items-center px-5 py-10">
+          <div className="flex w-full h-fit bg-red-500 rounded-full p-1 border-dashed border-2 border-cyan-300 shadow-md">
+            <img
+              src="../img/profileimg.jpg"
+              className="w-full h-fit object-cover rounded-full border-2 border-slate-400 aspect-square"
+              alt="Profile"
+            />
+          </div>
+        </div>
+        <div className="flex w-3/4 h-screen bg-red-50"></div>
+      </div>
+    </main>
   );
 }
