979e8c7bd9f975e891682b97dcc898afd8121eae
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-09-24 21:21:24 +0800

    added section in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center bg-teal-950 h-screen w-screen sm:items-start">
+    <main className="flex flex-col gap-8 row-start-2 items-center bg-teal-950 h-full w-screen sm:items-start">
       <div className="flex flex-row h-screen max-w-7xl w-screen mx-auto bg-cyan-200">
         <div className="flex flex-col w-1/4 h-screen  bg-slate-950 border-2 rounded-md border-cyan-500 justify-start items-center px-5 py-10">
           <div className="flex w-full h-fit bg-red-500 rounded-full p-1 border-dashed border-2 border-cyan-300 shadow-md">
@@ -12,7 +12,141 @@ export default function Home() {
             <h1 className="font-heading font-extrabold "></h1>
           </div>
         </div>
-        <div className="flex w-3/4 h-screen bg-red-50"></div>
+        <div className="flex flex-col w-3/4 h-screen ">
+          <div className="w-full sticky h-14 bg-blue-500 z-50 mx-auto">
+            sadas
+          </div>
+          <div className="h-screen  w-full bg-gray-950 overflow-scroll scroll-my-1 p-10">
+            <p>
+              Paragraph example 1: about Bali Bali is predominantly a Hindu
+              country. Bali is known for its elaborate, traditional dancing. The
+              dancing is inspired by its Hindi beliefs. Most of the dancing
+              portrays tales of good versus evil. To watch the dancing is a
+              breathtaking experience. Lombok has some impressive points of
+              interest – the majestic Gunung Rinjani is an active volcano. It is
+              the second highest peak in Indonesia. Art is a Balinese passion.
+              Batik paintings and carved statues make popular souvenirs. Artists
+              can be seen whittling and painting on the streets, particularly in
+              Ubud. It is easy to appreciate each island as an attractive
+              tourist destination. Majestic scenery; rich culture; white sands
+              and warm, azure waters draw visitors like magnets every year.
+              Snorkelling and diving around the nearby Gili Islands is
+              magnificent. Marine fish, starfish, turtles and coral reef are
+              present in abundance. Bali and Lombok are part of the Indonesian
+              archipelago. Bali has some spectacular temples. The most
+              significant is the Mother Temple, Besakih. The inhabitants of
+              Lombok are mostly Muslim with a Hindu minority. Lombok remains the
+              most understated of the two islands. Lombok has several temples
+              worthy of a visit, though they are less prolific. Bali and Lombok
+              are neighbouring islands. Paragraph about Bali: possible solution
+              Paragraph example 1: about Bali Bali is predominantly a Hindu
+              country. Bali is known for its elaborate, traditional dancing. The
+              dancing is inspired by its Hindi beliefs. Most of the dancing
+              portrays tales of good versus evil. To watch the dancing is a
+              breathtaking experience. Lombok has some impressive points of
+              interest – the majestic Gunung Rinjani is an active volcano. It is
+              the second highest peak in Indonesia. Art is a Balinese passion.
+              Batik paintings and carved statues make popular souvenirs. Artists
+              can be seen whittling and painting on the streets, particularly in
+              Ubud. It is easy to appreciate each island as an attractive
+              tourist destination. Majestic scenery; rich culture; white sands
+              and warm, azure waters draw visitors like magnets every year.
+              Snorkelling and diving around the nearby Gili Islands is
+              magnificent. Marine fish, starfish, turtles and coral reef are
+              present in abundance. Bali and Lombok are part of the Indonesian
+              archipelago. Bali has some spectacular temples. The most
+              significant is the Mother Temple, Besakih. The inhabitants of
+              Lombok are mostly Muslim with a Hindu minority. Lombok remains the
+              most understated of the two islands. Lombok has several temples
+              worthy of a visit, though they are less prolific. Bali and Lombok
+              are neighbouring islands. Paragraph about Bali: possible solution
+              Paragraph example 1: about Bali Bali is predominantly a Hindu
+              country. Bali is known for its elaborate, traditional dancing. The
+              dancing is inspired by its Hindi beliefs. Most of the dancing
+              portrays tales of good versus evil. To watch the dancing is a
+              breathtaking experience. Lombok has some impressive points of
+              interest – the majestic Gunung Rinjani is an active volcano. It is
+              the second highest peak in Indonesia. Art is a Balinese passion.
+              Batik paintings and carved statues make popular souvenirs. Artists
+              can be seen whittling and painting on the streets, particularly in
+              Ubud. It is easy to appreciate each island as an attractive
+              tourist destination. Majestic scenery; rich culture; white sands
+              and warm, azure waters draw visitors like magnets every year.
+              Snorkelling and diving around the nearby Gili Islands is
+              magnificent. Marine fish, starfish, turtles and coral reef are
+              present in abundance. Bali and Lombok are part of the Indonesian
+              archipelago. Bali has some spectacular temples. The most
+              significant is the Mother Temple, Besakih. The inhabitants of
+              Lombok are mostly Muslim with a Hindu minority. Lombok remains the
+              most understated of the two islands. Lombok has several temples
+              worthy of a visit, though they are less prolific. Bali and Lombok
+              are neighbouring islands. Paragraph about Bali: possible solution
+              Paragraph example 1: about Bali Bali is predominantly a Hindu
+              country. Bali is known for its elaborate, traditional dancing. The
+              dancing is inspired by its Hindi beliefs. Most of the dancing
+              portrays tales of good versus evil. To watch the dancing is a
+              breathtaking experience. Lombok has some impressive points of
+              interest – the majestic Gunung Rinjani is an active volcano. It is
+              the second highest peak in Indonesia. Art is a Balinese passion.
+              Batik paintings and carved statues make popular souvenirs. Artists
+              can be seen whittling and painting on the streets, particularly in
+              Ubud. It is easy to appreciate each island as an attractive
+              tourist destination. Majestic scenery; rich culture; white sands
+              and warm, azure waters draw visitors like magnets every year.
+              Snorkelling and diving around the nearby Gili Islands is
+              magnificent. Marine fish, starfish, turtles and coral reef are
+              present in abundance. Bali and Lombok are part of the Indonesian
+              archipelago. Bali has some spectacular temples. The most
+              significant is the Mother Temple, Besakih. The inhabitants of
+              Lombok are mostly Muslim with a Hindu minority. Lombok remains the
+              most understated of the two islands. Lombok has several temples
+              worthy of a visit, though they are less prolific. Bali and Lombok
+              are neighbouring islands. Paragraph about Bali: possible solution
+              Paragraph example 1: about Bali Bali is predominantly a Hindu
+              country. Bali is known for its elaborate, traditional dancing. The
+              dancing is inspired by its Hindi beliefs. Most of the dancing
+              portrays tales of good versus evil. To watch the dancing is a
+              breathtaking experience. Lombok has some impressive points of
+              interest – the majestic Gunung Rinjani is an active volcano. It is
+              the second highest peak in Indonesia. Art is a Balinese passion.
+              Batik paintings and carved statues make popular souvenirs. Artists
+              can be seen whittling and painting on the streets, particularly in
+              Ubud. It is easy to appreciate each island as an attractive
+              tourist destination. Majestic scenery; rich culture; white sands
+              and warm, azure waters draw visitors like magnets every year.
+              Snorkelling and diving around the nearby Gili Islands is
+              magnificent. Marine fish, starfish, turtles and coral reef are
+              present in abundance. Bali and Lombok are part of the Indonesian
+              archipelago. Bali has some spectacular temples. The most
+              significant is the Mother Temple, Besakih. The inhabitants of
+              Lombok are mostly Muslim with a Hindu minority. Lombok remains the
+              most understated of the two islands. Lombok has several temples
+              worthy of a visit, though they are less prolific. Bali and Lombok
+              are neighbouring islands. Paragraph about Bali: possible solution
+              Paragraph example 1: about Bali Bali is predominantly a Hindu
+              country. Bali is known for its elaborate, traditional dancing. The
+              dancing is inspired by its Hindi beliefs. Most of the dancing
+              portrays tales of good versus evil. To watch the dancing is a
+              breathtaking experience. Lombok has some impressive points of
+              interest – the majestic Gunung Rinjani is an active volcano. It is
+              the second highest peak in Indonesia. Art is a Balinese passion.
+              Batik paintings and carved statues make popular souvenirs. Artists
+              can be seen whittling and painting on the streets, particularly in
+              Ubud. It is easy to appreciate each island as an attractive
+              tourist destination. Majestic scenery; rich culture; white sands
+              and warm, azure waters draw visitors like magnets every year.
+              Snorkelling and diving around the nearby Gili Islands is
+              magnificent. Marine fish, starfish, turtles and coral reef are
+              present in abundance. Bali and Lombok are part of the Indonesian
+              archipelago. Bali has some spectacular temples. The most
+              significant is the Mother Temple, Besakih. The inhabitants of
+              Lombok are mostly Muslim with a Hindu minority. Lombok remains the
+              most understated of the two islands. Lombok has several temples
+              worthy of a visit, though they are less prolific. Bali and Lombok
+              are neighbouring islands. Paragraph about Bali: possible solution
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
