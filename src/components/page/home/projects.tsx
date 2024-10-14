0a32a06ae7dbe90a742fc11266b619699cb9feec
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-10-14 08:46:06 +0800

    fixed

--- src/components/page/home/projects.tsx
+++ src/components/page/home/projects.tsx
@@ -3,7 +3,7 @@ import ProjectsCard from "@/components/reusable/projectcard";
 export default function HomeProject() {
   return (
     <>
-      <div className="mx-auto max-w-normal min-h-normalh sm:min-h-screen flex flex-col p-10">
+      <div className="mx-auto max-w-normal min-h-normalh xl:h-normalh md:h-normalh sm:min-h-screen flex flex-col p-10">
         <div className="mb-5">
           <h1 className="md:text-7xl font-extrabold text-dark drop-shadow-sm sm:text-3xl">
             Projects
