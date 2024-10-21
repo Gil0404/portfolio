677a066ebb7de7edd1e7fee4351dc9c067cd5f39
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-10-21 11:44:20 +0800

    added resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,17 +6,14 @@ export default function Resume() {
     <>
       <section className="snap-y snap-mandatory overflow-x-hidden overflow-y-scroll h-screen">
         <Navbar></Navbar>
-        <div className="md:pt-16 pt-16 sm:pt-0 snap-always  h-screen snap-start bg-slate-500 text-high text-7xl">
-          ssss
-        </div>
-        <div className="md:pt-16 pt-16 sm:pt-0 snap-always  snap-start bg-red-950 text-high text-7xl">
-          <div className=" h-normalh w-screen bg-black"></div>
-        </div>
-        <div className="md:pt-16 pt-16 sm:pt-0 snap-always h-screen snap-start bg-green-950 text-high text-7xl">
-          dd
+        <div className="md:pt-20 pt-16 sm:pt-0 snap-always max-w-normal mx-auto h-screen snap-start bg-slate-500 text-high text-7xl">
+          <iframe
+            src="/GilbertGarcia.pdf"
+            className=" w-full h-screen bg-red-500 text-orange-700 "
+            style={{ background: "#ffffff" }}
+          ></iframe>
         </div>
       </section>
-      <div className="bg-yellow-200 w-screen h-normalh">f</div>
     </>
   );
 }
